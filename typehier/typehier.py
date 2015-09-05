b2098ee5917a65dc45bc320ee17df3030f59f632
William Lupton <wfl@cantab.net>
2015-09-05 22:43:24 +0100

    Initial version that doesn't actually do anything

---
+++ typehier/typehier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Investigation of data model type hierarchies."""
+
+class Object:
+    """Base class. All objects have unique names."""
+
+    _objects = []
+    
+    def __init__(self, name):
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
