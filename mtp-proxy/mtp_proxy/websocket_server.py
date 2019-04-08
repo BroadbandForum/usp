be808602706f7a55da73dbb6865cec4591922a6f
John Blackford <john.blackford@arris.com>
2019-04-08 09:57:47 -0500

    Clean up dead code

--- mtp-proxy/mtp_proxy/websocket_server.py
+++ mtp-proxy/mtp_proxy/websocket_server.py
@@ -50,42 +50,6 @@ import simple_websocket_server
 
 from mtp_proxy import utils
 
-# TODO: Create MySimpleWebSocketServer(simple_websocket_server.WebSocketServer)
-### __init__ also accepts a queue
-### Have it expose a get_websocket() - get first in the list and warn if more than 1; return None if list is empty
-### Expand _construct_websocket() to call set_queue after the WebSocket is constructed
-
-#my_global_cli_conn = None
-#my_global_queue = utils.GenericReceivingQueue()
-
-
-#def get_queue():
-#    return my_global_queue
-
-
-#def add_item_to_queue(item):
-#    my_global_queue.push(item)
-
-
-#def client_connected(web_socket_conn):
-#    global my_global_cli_conn
-#    my_global_cli_conn = web_socket_conn
-
-
-#def client_disconnected():
-#    global my_global_cli_conn
-#    my_global_cli_conn = None
-
-
-#def is_websocket_client_connected():
-#    global my_global_cli_conn
-#    return my_global_cli_conn is not None
-
-
-#def get_websocket_client_connection():
-#    global my_global_cli_conn
-#    return my_global_cli_conn
-
 
 class WebSocketListeningThread(threading.Thread):
     """A Thread that executes the AsyncIO Event Loop Processing to receive CoAP messages"""
@@ -97,7 +61,6 @@ class WebSocketListeningThread(threading.Thread):
         self._path = path
         self._queue = queue
         self._debug = debug
-#        self._client = None
         self._server = None
         self._logger = logging.getLogger(self.__class__.__name__)
 
@@ -107,15 +70,12 @@ class WebSocketListeningThread(threading.Thread):
 
         if client is not None:
             client.send_message(payload)
-#        if is_websocket_client_connected():
-#            get_websocket_client_connection().send_message(payload)
             self._logger.info("Message [%s] Sent!", payload)
         else:
             self._logger.warning("No Client connected, skipping message")
 
     def run(self):
         """Listen for incoming CoAP messages for the Resources provided"""
-#        self._server = simple_websocket_server.WebSocketServer(self._host, self._port, WebSocketServerHandler)
         self._server = ExtendedSimpleWebSocketServer(self._host, self._port, self._queue, WebSocketServerHandler)
         self._logger.info("Establishing: ws://%s:%d", self._host, self._port)
         self._server.serve_forever()
@@ -123,15 +83,6 @@ class WebSocketListeningThread(threading.Thread):
 
 class WebSocketServerHandler(simple_websocket_server.WebSocket):
     """A WebSocket Server Handler : defining __init__ seems to always break things, so avoiding it"""
-#    def __init__(self, server, sock, address):
-#        """Extend the WebSocket to implement the Handler methods and use a Queue"""
-#        simple_websocket_server.WebSocket.__init__(self, server, sock, address)
-#        self._queue = None
-#        self._logger = logging.getLogger(self.__class__.__name__)
-
-#    def set_queue(self, queue):
-#        """Set the Queue to use when receiving a message"""
-#        self._queue = queue
 
     def handle(self):
         """Handle incoming WebSocket messages; add them to the global Queue"""
@@ -144,20 +95,17 @@ class WebSocketServerHandler(simple_websocket_server.WebSocket):
             self._queue.push(queue_item)
         else:
             logger.warning("Queue has not been set: incoming message dropped")
-#        add_item_to_queue(queue_item)
 
     def connected(self):
         """Handle notification of a new WebSocket Client Connecting"""
         logger = logging.getLogger(self.__class__.__name__)
         logger.info("New client connected: %s", self.address)
         self._queue = self.server.get_queue()
-#        client_connected(self)
 
     def handle_close(self):
         """Handle notification of a WebSocket Client Disconnecting"""
         logger = logging.getLogger(self.__class__.__name__)
         logger.warning("Client disconnected: %s", self.address)
-#        client_disconnected()
 
 
 class ExtendedSimpleWebSocketServer(simple_websocket_server.WebSocketServer):
@@ -166,16 +114,10 @@ class ExtendedSimpleWebSocketServer(simple_websocket_server.WebSocketServer):
         """Initialize the Extended Simple WebSocket Server"""
         simple_websocket_server.WebSocketServer.__init__(self, host, port, websocketclass)
         self._queue = queue
-#        self._logger = logging.getLogger(self.__class__.__name__)
 
     def get_queue(self):
         """Retrieve the Queue"""
         return self._queue
-
-#    def _construct_websocket(self, sock, address):
-#        """Extend the WebSocket construction method to provide a Queue to the WebSocket Handler"""
-#        ws = simple_websocket_server.WebSocketServer._construct_websocket(self, sock, address)
-#        ws.set_queue(self._queue)
 
     def get_connected_client(self):
         """There should only be 1 connected WebSocket client, so retrieve it"""
@@ -184,8 +126,6 @@ class ExtendedSimpleWebSocketServer(simple_websocket_server.WebSocketServer):
 
         if len(self.connections) > 0:
             connected_client = next(iter(self.connections.values()))
-#            for connected_client in self.connections.values():
-#                break
 
         if len(self.connections) > 1:
             logger.warning("Attempting to retrieve the connected WebSocket; Found more than 1!")
@@ -207,7 +147,6 @@ class WebSocketServer:
 
     def get_msg(self, timeout_in_seconds=-1):
         """Retrieve a Queue Item from the queue"""
-#        return get_queue().get_msg(timeout_in_seconds)
         return self._queue.get_msg(timeout_in_seconds)
 
     def send_msg(self, payload):
@@ -217,6 +156,5 @@ class WebSocketServer:
     def listen(self):
         """Listen to events from the WebSocket connection"""
         self._logger.info("Starting the WebSocket Listening Thread...")
-#        self._listen_thr = WebSocketListeningThread(self._host, self._port, self._path)
         self._listen_thr = WebSocketListeningThread(self._host, self._port, self._path, self._queue)
         self._listen_thr.start()
